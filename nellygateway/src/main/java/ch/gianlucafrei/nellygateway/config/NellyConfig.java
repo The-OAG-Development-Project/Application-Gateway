32ce929dcbf318d74ed82b40079cae3743d25173
Gian-Luca Frei <gifr@Gian-Lucas-MacBook-Pro.local>
2020-10-23 12:44:32 +0200

    Added programmatic zuul configuration

--- nellygateway/src/main/java/ch/gianlucafrei/nellygateway/config/NellyConfig.java
+++ nellygateway/src/main/java/ch/gianlucafrei/nellygateway/config/NellyConfig.java
@@ -2,14 +2,18 @@ package ch.gianlucafrei.nellygateway.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class NellyConfig {
 
     public Map<String, AuthProvider> authProviders;
+    public Map<String, NellyRoute> routes;
     public String hostUri;
     public String nellyApiKey;
 
@@ -28,6 +32,22 @@ public class NellyConfig {
         NellyConfig config = om.readValue(file, NellyConfig.class);
 
         return config;
+    }
+
+    public Map<String, ZuulProperties.ZuulRoute> getRoutesAsZuulRoutes(){
+
+        Map<String, ZuulProperties.ZuulRoute> zuulRoutes = new HashMap<>();
+
+        if(routes != null)
+        {
+            routes.forEach((name, route) -> {
+                ZuulProperties.ZuulRoute zuulRoute = new ZuulProperties.ZuulRoute(route.path, route.url);
+                zuulRoute.setSensitiveHeaders(new HashSet<>());
+                zuulRoutes.put(name, zuulRoute);
+            });
+        }
+
+        return zuulRoutes;
     }
 
 }
